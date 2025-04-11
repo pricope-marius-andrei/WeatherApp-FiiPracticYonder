9f6d689a7b686e872a22428d9cc3444adc62b609
pricope-marius-andrei <pricopemarius33@gmail.com>
2025-04-11 10:53:39 +0300

    refactor request history controller

--- src/main/java/com/example/weatherapp/controller/RequestHistoryController.java
+++ src/main/java/com/example/weatherapp/controller/RequestHistoryController.java
@@ -1,23 +1,14 @@
 package com.example.weatherapp.controller;
 
 import com.example.weatherapp.dto.UserDto;
-import com.example.weatherapp.model.RequestHistory;
-import com.example.weatherapp.model.UserModel;
 import com.example.weatherapp.dto.RequestHistoryDto;
-import com.example.weatherapp.dto.WeatherDto;
-import com.example.weatherapp.mapper.UserMapper;
 import com.example.weatherapp.service.RequestHistoryServiceImpl;
-import com.example.weatherapp.service.WeatherServiceImpl;
-import com.example.weatherapp.service.interfaces.EmailService;
 import com.example.weatherapp.service.interfaces.RequestHistoryService;
 import com.example.weatherapp.service.interfaces.UserService;
-import com.example.weatherapp.service.interfaces.WeatherService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
@@ -26,48 +17,12 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/request-history")
 public class RequestHistoryController {
 
-    private final WeatherService weatherService;
     private final RequestHistoryService requestHistoryService;
-    private final EmailService emailService;
     private final UserService userService;
-    private final UserMapper userMapper;
 
-    public RequestHistoryController(WeatherServiceImpl weatherServiceImpl, RequestHistoryServiceImpl requestHistoryServiceImpl, EmailService emailService, UserService userService, UserMapper userMapper) {
-        this.weatherService = weatherServiceImpl;
+    public RequestHistoryController(RequestHistoryServiceImpl requestHistoryServiceImpl, UserService userService) {
         this.requestHistoryService = requestHistoryServiceImpl;
-        this.emailService = emailService;
         this.userService = userService;
-        this.userMapper = userMapper;
-    }
-
-    @PostMapping
-    public ResponseEntity<String> addRequestHistory(@AuthenticationPrincipal UserDetails userDetails, @RequestParam Double latitude, @RequestParam Double longitude) {
-
-        WeatherDto weatherDto = weatherService.getWeatherDetails(latitude, longitude);
-
-
-        UserModel userModel = userMapper.toEntity(userService.getUserByUsername(userDetails.getUsername()));
-
-        if(userModel == null)
-        {
-            return new ResponseEntity<>( "User not found", HttpStatus.NOT_FOUND);
-        }
-
-        emailService.sendEmail(userModel.getUsername() + "@gmail.com", "Weather Request", weatherDto.toString());
-
-        userModel.setVersion(0L);
-
-        RequestHistory requestHistory = new RequestHistory();
-
-        requestHistory.setUser(userModel);
-        requestHistory.setLat(latitude);
-        requestHistory.setLon(longitude);
-        requestHistory.setResponse(weatherDto.toString());
-
-        userModel.addRequest(requestHistory);
-
-        requestHistoryService.addRequestHistory(requestHistory);
-        return new ResponseEntity<>( "Request history added successfully", HttpStatus.CREATED);
     }
 
     @GetMapping
@@ -83,11 +38,4 @@ public class RequestHistoryController {
 
         return requestHistoryService.getPagedRequestHistoriesByUserId(user.getId(), pageable);
     }
-
-    @GetMapping("/details")
-    public ResponseEntity<WeatherDto> getDetails(@RequestParam double lat, @RequestParam double lon) {
-        WeatherDto response = weatherService.getWeatherDetails(lat, lon);
-        return ResponseEntity.ok(response);
-    }
-
 }
