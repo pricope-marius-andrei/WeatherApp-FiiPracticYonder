3c13ffdd2e2de601403af581924743dfd484d8d4
pricope-marius-andrei <pricopemarius33@gmail.com>
2025-04-24 10:22:52 +0300

    add request history controller test, and add some request history for the first mock user

---
+++ src/test/java/com/example/weatherapp/controller/RequestHistoryControllerTest.java
@@ -0,0 +1,84 @@
+package com.example.weatherapp.controller;
+
+import com.example.weatherapp.dto.RequestHistoryDto;
+import com.example.weatherapp.dto.UserDto;
+import com.example.weatherapp.jwt.JwtFilter;
+import com.example.weatherapp.service.interfaces.RequestHistoryService;
+import com.example.weatherapp.service.interfaces.UserService;
+import com.example.weatherapp.utils.TestDataUtil;
+import lombok.experimental.WithBy;
+import org.apache.catalina.security.SecurityConfig;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.security.Principal;
+import java.util.List;
+import java.util.Set;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(RequestHistoryController.class)
+@Import(SecurityConfig.class)
+@AutoConfigureMockMvc(addFilters = false)
+public class RequestHistoryControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private RequestHistoryService requestHistoryService;
+
+    @MockitoBean
+    private UserService userService;
+
+    @MockitoBean
+    private UserDetails userDetails;
+
+    @MockitoBean
+    private JwtFilter jwtFilter;
+
+    @Test
+    @WithMockUser(username = "username1")
+    public void testGetAllRequests() throws Exception {
+        Pageable pageable = PageRequest.of(0, 2, Sort.by("id").ascending());
+
+        UserDto user = TestDataUtil.createTestUsers().getFirst();
+
+        when(userService.getUserByUsername(user.getUsername())).thenReturn(user);
+
+        Set<RequestHistoryDto> requestHistories = user.getRequestHistories();
+
+        final int start = (int) pageable.getOffset();
+        final int end = Math.min((start + pageable.getPageSize()), requestHistories.size());
+
+        when(requestHistoryService.getPagedRequestHistoriesByUserId(eq(user.getId()), any(Pageable.class)))
+                .thenReturn(new PageImpl<>(List.copyOf(requestHistories).subList(start, end), pageable, requestHistories.size()));
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/request-history")
+                        .param("page", "0")
+                        .param("size", "2")
+                        .param("sortBy", "id"))
+                        .andExpect(status().isOk())
+                        .andExpect(jsonPath("$.size()").value(pageable.getPageSize()));
+
+    }
+
+}
--- src/test/java/com/example/weatherapp/utils/TestDataUtil.java
+++ src/test/java/com/example/weatherapp/utils/TestDataUtil.java
@@ -1,16 +1,20 @@
 package com.example.weatherapp.utils;
 
+import com.example.weatherapp.dto.RequestHistoryDto;
 import com.example.weatherapp.dto.UserDto;
 import com.example.weatherapp.dto.UserProfileDto;
 
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Set;
 
 public class TestDataUtil {
 
     public static List<UserDto> createTestUsers() {
 
+
         return List.of(
-                new UserDto(1L, "User1", "username1", new UserProfileDto(1L, "username1@gmail.com", false), null),
+                new UserDto(1L, "User1", "username1", new UserProfileDto(1L, "username1@gmail.com", false), createTestRequestHistories()),
                 new UserDto(2L, "User2", "username2", null, null),
                 new UserDto(3L, "User3", "username3", null, null),
                 new UserDto(4L, "User4", "username4", null, null)
@@ -23,6 +27,15 @@ public class TestDataUtil {
                 new UserProfileDto(2L, "username2@gmail.com", false),
                 new UserProfileDto(3L, "username3@gmail.com", true),
                 new UserProfileDto(4L, "username4@gmail.com", false)
+        );
+    }
+
+    private static Set<RequestHistoryDto> createTestRequestHistories() {
+        return Set.of(
+               new RequestHistoryDto(1L, 2, 12, "response1", false, true, 12, false),
+               new RequestHistoryDto(2L, 3, 13, "response2", false, true, 13, false),
+               new RequestHistoryDto(3L, 4, 14, "response3", false, true, 14, false),
+               new RequestHistoryDto(4L, 5, 15, "response4", false, true, 15, false)
         );
     }
 }
